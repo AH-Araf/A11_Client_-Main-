89090fd938fb0d7f71026fd91ed1304fe9ad3311
Araf <arafathossain.baiust@gmail.com>
2022-11-10 19:49:02 +0600

    blog section added

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -12,7 +12,6 @@ const Blog = () => {
                 <h4><span className='que'>Question:</span> What is JWT, and how does it work?</h4>
                 <p><span className='ans'>Answer:</span> JSON Web Token (JWT) is an open standard for securely transmitting information between parties as JSON object. It is compact, readable and digitally signed using a private key/ or a public key pair by the Identity Provider.</p>
             </div>
-
             <div className='bottom'>
                 <h4><span className='que'>Question:</span> What is the difference between javascript and NodeJS?</h4>
                 <p><span className='ans'>Answer:</span> JavaScript is a simple programming language that can be used with any browser that has the JavaScript Engine installed. Node. js, on the other hand, is an interpreter or execution environment for the JavaScript programming language.</p>
